255527bb84b13c4e881df06a3a552dfcb0014b86
hyeongeun <hhg7899@gmail.com>
2023-08-01 15:06:43 +0900

    SQLite

---
+++ Hello/HelloKotlin/src/main/java/com/example/hellokotlin/Step11_If.kt
@@ -0,0 +1,42 @@
+package com.example.hellokotlin
+
+import java.util.Scanner
+
+fun main() {
+    // Scanner 객체를 생성해서
+    val scan = Scanner(System.`in`)
+    // 숫자를 하나 입력받는다
+    print("숫자 입력: ")
+    val inputNum = scan.nextInt()
+
+    var result: String
+    // 입력한 숫자가 홀수인지 짝수인지 판별
+    if (inputNum % 2 == 1) {
+        result = "$inputNum 은 홀수입니다."
+    } else {
+        result = "$inputNum 은 짝수입니다"
+    }
+
+    // 값을 리턴하는 if 문
+    val result2 = if (inputNum % 2 == 1) {
+        "$inputNum 은 홀수입니다."
+    } else {
+        "$inputNum 은 짝수입니다."
+    }
+    // 좀 더 간략히 표현하면
+    val result3 = if (inputNum % 2 == 1) "$inputNum 은 홀수입니다." else "$inputNum 은 짝수입니다."
+
+    val result4 = if (inputNum % 2 == 1) {
+        println("작업중...")
+        // 만일 if 문의 블럭 내에 여러 줄이 있다면 마지막 줄이 리턴된다.
+        "$inputNum 은 홀수입니다."
+    } else {
+        println("작업중...")
+        "$inputNum 은 짝수입니다."
+    }
+
+    println(result)
+    println(result2)
+    println(result3)
+    println(result4)
+}
---
+++ Hello/HelloKotlin/src/main/java/com/example/hellokotlin/java/Sender.java
@@ -0,0 +1,7 @@
+package com.example.hellokotlin.java;
+
+public class Sender {
+    public void send(String msg) {
+        System.out.println(msg + " 를 전송합니다.");
+    }
+}
